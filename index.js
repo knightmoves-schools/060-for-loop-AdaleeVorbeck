ffe02407a546919ec72bc4aab1f128f55df8be88
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-12 14:15:49 -0600

    YEAH ITS ALL GONNA BE ALRIGHT

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let todos = ['Laundry', 'Dishes', 'Clean Desk', 'Sweep Floor'];
+
+function markAsDone(todos) {
+  for (let i = 0; i < todos.length; i++) {
+    todos[i] = 'done - ' + todos[i];
+  }
+  return todos;
+}
+
+document.getElementById('result').innerHTML = markAsDone(todos).join(', ');
 
